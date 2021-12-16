66148b39e687d2750610a649738fec114091308a
Shlomi Nissan <shlomi@betamark.com>
2021-12-16 01:35:58 -0800

    Add gflags to parse program arguments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string_view>
+#include <gflags/gflags.h>
+
+const auto valid_path_message = "Specify a path to a valid Chip-8 program";
+DEFINE_string(rom, "", valid_path_message);
+
+bool validateRom(std::string_view value) {
+    return !value.empty();
+}
 
 int main(int argc, char** argv) {
-    
-    return 0;
+    gflags::SetUsageMessage("chip8 -rom <PATH>");
+    gflags::SetVersionString("1.0.0");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (!validateRom(FLAGS_rom)) {
+        std::cerr << valid_path_message << "\n" << "Usage: chip8 -rom <PATH>\n";
+        return EXIT_FAILURE;
+    }
+
+    // TODO: instantiate emulator
+    // TODO: load rom
+    // TODO: run
+
+    return EXIT_SUCCESS;
 }
