2f1a8c190e765e1a49d81b546497826366273332
Shlomi Nissan <shlomi@betamark.com>
2021-12-29 16:18:16 -0800

    Integrate GoogleTest

---
+++ tests/instructions_test.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include <algorithm>
+
+#include "core/instructions.h"
+#include "core/cpu.h"
+#include "core/display.h"
+
+using namespace c8;
+
+TEST(Instructions, OP_CLS) {
+    Display display;
+
+    // turn pixels on
+    display[0x01] = 1;
+    display[0xFF] = 1;
+
+    instruction::OP_CLS(&display);
+
+    // make sure all pixels are turned off
+    for (auto i = 0; i < Display::kWidth * Display::kHeight; ++i) {
+        EXPECT_EQ(display[i], 0);
+    }
+}
+
+TEST(Instructions, OP_RET) {
+    Cpu cpu;
+
+    // call subroutine 0x001
+    instruction::OP_CALL(0x2001, &cpu);
+
+    // return from subroutine (empty call stack)
+    instruction::OP_RET(&cpu);
+    EXPECT_EQ(cpu.sp, 0);
+    EXPECT_EQ(cpu.stack[cpu.sp], 0);
+}
+
+TEST(Instructions, OP_JP) {
+    Cpu cpu;
+
+    // jump to address 0x011A
+    instruction::OP_JP(0x111A, &cpu);
+    EXPECT_EQ(cpu.pc, 0x011A);
+}
